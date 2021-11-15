6b42ba3f6874a1d04fa3fae638b876ce9ab0b27a
soma1809 <60248071+soma1809@users.noreply.github.com>
2021-11-15 16:14:22 +0530

    Update addresses.c

--- addresses.c
+++ addresses.c
@@ -1,24 +1,31 @@
+//Include the header files
 #include<stdio.h>
 #include<stdlib.h>
 
+//Function to obtain page number from 32-bit virtual address 
 unsigned int addr_pn(unsigned int address){
   return address>>12;
 }
 
+//Function to obtain offset from 32-bit virtual address 
 unsigned int addr_of(unsigned int address){
   unsigned int mask=4095;
   return (address & mask);
 }
   
 int main(int argc, char* argv[]){
+  
+  //variable declerations
   unsigned int address;
   unsigned int page_number;
   unsigned int offset;
   
-  address=(unsigned int)atoi(argv[1]);
+  address=(unsigned int)atoi(argv[1]); //Use atoi to convert virtual address from string type to int
+  //Function calls
   page_number=addr_pn(address);
   offset=addr_of(address);
 
+  // Print the output
   printf("The address %u contains:\n",address);
   printf("page number = %u\n",page_number);
   printf("offset = %u\n",offset);
